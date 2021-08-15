d333291c23ccec74cef46493381cb041a05362ae
Anurag Singh <singh.anurag1905@gmail.com>
2021-08-15 10:49:54 +0530

    add basic run functionality

---
+++ src/app/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  identity: "",
+  isRoomHost: "",
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
