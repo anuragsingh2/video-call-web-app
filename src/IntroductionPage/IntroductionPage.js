8330ff364f81235a9808be5a8b53731566845a79
Anurag Singh <singh.anurag1905@gmail.com>
2021-08-16 17:08:09 +0530

    Add Introduction Page

--- src/IntroductionPage/IntroductionPage.js
+++ src/IntroductionPage/IntroductionPage.js
@@ -1,8 +1,17 @@
 import React from "react";
+import logo from "../resources/images/logo.png";
+import ConnectingButtons from "./ConnectingButtons";
 import "./IntroductionPage.css";
 
 const IntroductionPage = () => {
-  return <div>Introduction Page</div>;
+  return (
+    <div className="introduction_page_container">
+      <div className="introduction_page_panel">
+        <img src={logo} className="introduction_page_image" />
+        <ConnectingButtons />
+      </div>
+    </div>
+  );
 };
 
 export default IntroductionPage;
