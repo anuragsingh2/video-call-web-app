3a627b24043422842173d3e8101ad5a0ddb87e5a
Anurag Singh <singh.anurag1905@gmail.com>
2021-08-19 14:24:31 +0530

    Add LeaveRoomButton

--- src/RoomPage/VideoSection/LeaveRoomButton.js
+++ src/RoomPage/VideoSection/LeaveRoomButton.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-const LeaveRoomButton = () => {
-  return <div></div>;
+const LeaveRoomButton = ({ room }) => {
+  const handleRoomDisconnection = () => {
+    // handle disconnection with room
+    const siteUrl = window.location.origin;
+    window.location.href = siteUrl;
+  };
+
+  return (
+    <div className="video_button_container">
+      <button className="video_button_end" onClick={handleRoomDisconnection}>
+        Leave Room
+      </button>
+    </div>
+  );
 };
 
 export default LeaveRoomButton;
