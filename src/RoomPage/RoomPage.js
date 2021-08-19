fe8113818afc584649c97ab40a95393568ef7d8f
Anurag Singh <singh.anurag1905@gmail.com>
2021-08-19 12:59:27 +0530

    Add ParticipantsSection

--- src/RoomPage/RoomPage.js
+++ src/RoomPage/RoomPage.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./RoomPage.css";
+import ParticipantsSection from "./ParticipantsSection/ParticipantsSection";
+import VideoSection from "./VideoSection/VideoSection";
+import ChatSection from "./ChatSection/ChatSection";
+
 const RoomPage = () => {
-  return <div>RoomPage</div>;
+  return (
+    <div className="room_container">
+      <ParticipantsSection />
+      <VideoSection />
+      <ChatSection />
+    </div>
+  );
 };
 
 export default RoomPage;
