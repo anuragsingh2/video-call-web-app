8c937475a96317a852b298b9b972c988a278117e
Anurag Singh <singh.anurag1905@gmail.com>
2021-08-19 14:12:33 +0530

    Add MicButton

--- src/RoomPage/VideoSection/MicButton.js
+++ src/RoomPage/VideoSection/MicButton.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import MicButtonImg from "../../resources/images/mic.svg";
+import MicButtonImgOff from "../../resources/images/micOff.svg";
 
 const MicButton = () => {
-  return <div></div>;
+  const [isMicMuted, setIsMicMuted] = useState(false);
+
+  const handleMicButtonPressed = () => {
+    isMicMuted ? unmute() : mute();
+    setIsMicMuted(!isMicMuted);
+  };
+
+  const mute = () => {
+    // mute our microphone so other users will not be able to hear us
+  };
+
+  const unmute = () => {
+    // turn on mic back
+  };
+
+  return (
+    <div className="video_button_container">
+      <img
+        src={isMicMuted ? MicButtonImgOff : MicButtonImg}
+        onClick={handleMicButtonPressed}
+        className="video_button_image"
+      />
+    </div>
+  );
 };
 
 export default MicButton;
